e8a17db39022858d1313678590d7c5d1b6b6f408
Aethys256 <aethys256@users.noreply.github.com>
2017-04-24 02:31:34 +0200

    [FastLoot] Fix Busy (For real!)

--- FastLoot.lua
+++ FastLoot.lua
@@ -22,25 +22,23 @@
     -- Disable Default Auto Loot
     SetCVar("autoLootDefault", 0);
 
-    local EmptySlot, BusyError;
     local LootSlotHasItem, LootSlot = LootSlotHasItem, LootSlot;
+    local function GetLoots ()
+      local EmptySlot = 0;
+      for i = 1, GetNumLootItems() do
+        if LootSlotHasItem(i) then
+          LootSlot(i);
+        else
+          EmptySlot = EmptySlot + 1;
+        end
+      end
+      return EmptySlot;
+    end
     AC:RegisterForEvent(
       function ()
         if not IsShiftKeyDown() then
-          -- Loot all the items one time, if there is a busy error then we do it again.
-          repeat
-            BusyError = false;
-            EmptySlot = 0;
-            for i = 1, GetNumLootItems() do
-              if LootSlotHasItem(i) then
-                LootSlot(i);
-              else
-                EmptySlot = EmptySlot + 1;
-              end
-            end
-          until not BusyError;
           -- Window Close fallback in case the game forgot to do it on an empty window.
-          if EmptySlot == GetNumLootItems() then
+          if GetLoots() == GetNumLootItems() then
             CloseLoot(); 
           end
         end
@@ -48,9 +46,9 @@
       , "LOOT_OPENED"
     );
     AC:RegisterForEvent(
-      function (MessageType, Message)
+      function (Event, MessageType, Message)
         if Message == ERR_OBJECT_IS_BUSY then
-          BusyError = true;
+          GetLoots();
         end
       end
       , "UI_ERROR_MESSAGE"
