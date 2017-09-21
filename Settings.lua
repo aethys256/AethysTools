b11a75761526598fc4af5620f137423ea64bea5b
Continuity <continuity@gmx.net>
2017-09-22 00:25:15 +0200

    - set default settings back to non-ElvUI and showing INF

--- Settings.lua
+++ Settings.lua
@@ -11,8 +11,8 @@
     Nameplates = {
       TTD = {
         Enabled = true,
-		useElvUI = true, -- set this if you are using ElvUI nameplates
-		showINF = false,
+		useElvUI = false, -- set this if you are using ElvUI nameplates
+		showINF = true,
         -- TODO: Make a DB with all "default" offset loaded based on what addon the user has Enabled
         -- TODO: And make a command to input custom offset
         -- Default Nameplate
